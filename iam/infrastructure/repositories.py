cdda4b5db9bc1335f1ddccf0a5a06bec56dd3ca2
Rodrigo Chirinos <u202217804@upc.edu.pe>
2025-10-02 11:50:04 -0500

    feat(iam): add device repository

---
+++ iam/infrastructure/repositories.py
@@ -0,0 +1,41 @@
+from typing import Optional
+from venv import create
+
+import peewee
+
+from iam.domain.entities import Device
+from iam.infrastructure.models import Device as DeviceModel
+
+class DeviceRepository:
+    @staticmethod
+    def find_by_id_and_api_key(device_id: str, api_key: str) -> Optional[Device]:
+        """
+        Finds a device by its ID and API key.
+        :param device_id: The ID of the device.
+        :param api_key: The API key of the device.
+        :return: The device if found, otherwise None.
+        """
+        try:
+            device = DeviceModel.get((DeviceModel.device_id == device_id) & (DeviceModel.api_key == api_key))
+            return Device(device_id=device_id, api_key=api_key, created_at=device.created_at)
+        except peewee.DoesNotExist:
+            return None
+
+    @staticmethod
+    def get_or_create_test_device() -> Device:
+        device, _ = DeviceModel.get_or_create(
+            device_id="smart-band-001",
+            defaults={"api_key": "test-api-key-123", created-at="2025-10-02-07T10:00:00Z"}
+        )
+        return Device(device_id=device.device_id, api_key=device.api_key, created_at=device.created_at)
+
+
+
+
+
+
+
+
+
+
+
